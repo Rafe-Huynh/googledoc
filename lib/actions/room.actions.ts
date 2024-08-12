7f8183774ade038465155b6455f7e9d4de85c6c4
Thai Huynh <rafehuynh0510@gmail.com>
2024-08-12 18:55:45 -0500

    Collaborative room

--- lib/actions/room.actions.ts
+++ lib/actions/room.actions.ts
@@ -1,30 +1,45 @@
 'use server'
-import {nanoid} from 'nanoid'
+import { nanoid } from 'nanoid'
 import { title } from 'process'
 import { liveblocks } from '../liveblocks'
 import { revalidatePath } from 'next/cache'
 import { parseStringify } from '../utils'
 
-export const createDocument = async ({userId, email}: CreateDocumentParams) => {
+export const createDocument = async ({ userId, email }: CreateDocumentParams) => {
     const roomId = nanoid()
 
-try {
-    const metadata = {
-        creatorId: userId,
-        email,
-        title: 'Untitled'
+    try {
+        const metadata = {
+            creatorId: userId,
+            email,
+            title: 'Untitled'
+        }
+        const usersAccesses: RoomAccesses = {
+            [email]: ['room:write']
+
+        }
+        const room = await liveblocks.createRoom(roomId, {
+            metadata,
+            usersAccesses,
+            defaultAccesses: ['room:write']
+        });
+        revalidatePath('/')
+        return parseStringify(room)
+    } catch (error) {
+        console.log(`Error while creating a room: ${error}`)
     }
-    const usersAccesses: RoomAccesses = {
-        [email]: ['room:write']
+}
+export const getDocument = async ({ roomId, userId }: { roomId: string, userId: string }) => {
+    try {
+        const room = await liveblocks.getRoom(roomId)
+        // const hasAccess = Object.keys(room.usersAccesses).includes(userId)
+        // if (!hasAccess) {
+        //     throw new Error('you have no access to this room')
+
+        // }
+        return parseStringify(room)
+    } catch (error) {
+        console.log(`${error} while creating a room `)
     }
-    const room = await liveblocks.createRoom(roomId, {
-        metadata,
-        usersAccesses,
-        defaultAccesses: []
-      });
-    revalidatePath('/')
-    return parseStringify(room)
-} catch (error) {
-    console.log(`Error while creating a room: ${error}`)
-}
-}
+
+} 
