8751e92657783db16151bdcfe32a968b1680b1a8
Thai Huynh <rafehuynh0510@gmail.com>
2024-08-14 21:40:02 -0500

    live features leaving comment in text

--- components/CollaborativeRoom.tsx
+++ components/CollaborativeRoom.tsx
@@ -14,8 +14,7 @@ import { currentUser } from '@clerk/nextjs/server';
 import Image from 'next/image';
 import { updateDocument } from '@/lib/actions/room.actions';
   
-const CollaborativeRoom = ({roomId, roomMetadata} : CollaborativeRoomProps) => {
-  const currentUserType = "editor"
+const CollaborativeRoom = ({roomId, roomMetadata, users, currentUserType} : CollaborativeRoomProps) => {
   const [editing, setEditing] = useState(false)
   const [loading, setLoading] = useState(false)
   const [documentTitle, setDocumentTitle] = useState(roomMetadata.title)
@@ -110,7 +109,7 @@ const CollaborativeRoom = ({roomId, roomMetadata} : CollaborativeRoomProps) => {
           </SignedIn>
           </div>
       </Header>
-      <Editor />
+      <Editor roomId={roomId} currentUserType = {currentUserType}/>
           </div>
         </ClientSideSuspense>
       </RoomProvider>
